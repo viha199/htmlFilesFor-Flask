f934dffb0e878061727042e55dd2ac9fe9bc7bab
viha199 <140887511+viha199@users.noreply.github.com>
2026-02-03 09:41:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,83 +1,70 @@
-from flask import Flask, render_template,request,redirect,url_for
-from werkzeug.security import generate_password_hash,check_password_hash
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3 
-
-from flask_login import(
-    LoginManager,login_user,logout_user,login_required,UserMixin,current_user
-)
+from werkzeug.security import generate_password_hash, check_password_hash 
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required, UserMixin
 
 app = Flask(__name__)
-app.secret_key="super-secret-key"
+app.secret_key = 'secretKey'
 
-def get_db():
-    conn=sqlite3.connect("database.db")
-    conn.row_factory=sqlite3
-    return conn
+db = sqlite3.connect('database.db', check_same_thread=False)
+db.row_factory = sqlite3.Row
+cursor = db.cursor()
 
-db=get_db()
-db.execute("""
-    CREATE TABLE IF NOT EXISTS USERS(email TEXT PRIMARY KEY, password TEXT NOT NULL)
-""")
-
-db.execute("""CREATE TABLE IF NOT EXISTS PARTICIPANTS(
-           name TEXT,
-           email TEXT,
-           city TEXT,
-           country TEXT,
-           phone TEXT)""")
+cursor.execute('CREATE TABLE IF NOT EXISTS USERS (email TEXT PRIMARY KEY, password TEXT NOT NULL)')
+cursor.execute('CREATE TABLE IF NOT EXISTS PARTICIPANTS (name TEXT, email TEXT, city TEXT, country TEXT, phone TEXT)')
 db.commit()
-login_manager=LoginManager()
+
+login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view="login"
+login_manager.login_view = 'login'
 
 class User(UserMixin):
-    def __init__(self,email):
-        self.id=email
+    def __init__(self, email):
+        self.id = email
 
 @login_manager.user_loader
 def load_user(user_id):
-    db=get_db()
-    user=db.execute(
-        "SELECT email FROM USERS WHERE email=?",
-        (user_id)
-    ).fetchone()
-    return User(user["email"]) if user else None
-@app.route("/")
-@app.route("/home")
+    user = cursor.execute('SELECT * FROM USERS WHERE email = ?', (user_id,)).fetchone()
+    if user:
+        return User(user['email'])
+    else:
+        None 
+
+@app.route('/')
+def root():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    else:
+        return render_template("login.html")
+
+
+@app.route('/index')
+@login_required
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-connect=sqlite3.connect('database.db')
-connect.execute(
-    'CREATE TABLE IF NOT EXISTS PARTICIPANTS(name TEXT,email TEXT,city TEXT,country TEXT,phone TEXT)'
-)
-
-@app.route("/join",methods=['GET','POST'])
-
+@app.route('/join', methods = ['GET', 'POST'])
+@login_required
 def join():
-    if request.method=='POST':
-        name=request.form["name"]
-        email=request.form["email"]
-        city=request.form["city"]
-        country=request.form["country"]
-        phone=request.form["phone"]
-        with sqlite3.connect("database.db") as users:
-            cursor=users.cursor()
-            cursor.execute("INSERT INTO PARTICIPANTS" \
-            "(name,email,city,country,phone) VALUES" \
-            "(?,?,?,?,?)",(name,email,city,country,phone))
-            users.commit()
-        return render_template("index.html")
-    return render_template("join.html")
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        city = request.form['city']
+        country = request.form['country']
+        phone = request.form['phone']
 
-@app.route("/participants")
+        cursor.execute('INSERT INTO PARTICIPANTS (name, email, city, country, phone) VALUES (?, ?, ?, ?, ?)', (name, email, city, country, phone))
+        db.commit()
+        return redirect(url_for('participants'))
+    
+    return render_template('join.html')
+
+@app.route('/participants')
+@login_required 
 def participants():
-    connect=sqlite3.connect('database.db')
-    curser=connect.cursor()
-    curser.execute('SELECT*FROM PARTICIPANTS')
-    data=curser.fetchall()
+    data = cursor.execute('SELECT * FROM PARTICIPANTS').fetchall()
+    return render_template('participants.html', data=data)
 
-    return render_template("participants.html",data=data)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
